dce0a9b42f34f60f651f699b485a5963ee54ac2e
Dylan Baldini <Dylan-baldini@outlook.fr>
2019-01-09 17:39:07 +0100

    ajout d'une image pour une arme

--- src/Controller/ArmeController.php
+++ src/Controller/ArmeController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Arme;
-use App\Form\Arme1Type;
+use App\Entity\Fichier;
+use App\Form\ArmeType;
 use App\Repository\ArmeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 
 class ArmeController extends AbstractController
@@ -27,11 +29,38 @@ class ArmeController extends AbstractController
     public function new(Request $request): Response
     {
         $arme = new Arme();
-        $form = $this->createForm(Arme1Type::class, $arme);
+        $form = $this->createForm(ArmeType::class, $arme);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
+
+
+                $fichier = new Fichier();
+                /** @var Symfony\Component\HttpFoundation\File\UploadedFile $file */
+
+                $file=$form->get('imageAvInsertion')->getData();
+//                $file=$form['fichier']->getData();
+                $fileName = md5(uniqid());
+                // Move the file to the directory where brochures are stored
+                $fileNameExtension = $fileName . '.' . $file->guessExtension();
+
+                $file->move($this->getParameter('upload_directory'), $fileNameExtension);
+                $path_parts = pathinfo($fileNameExtension);
+
+                $fichier->setContenueFichier($fileNameExtension);
+                $fichier->setFichierExtension($path_parts['extension']);
+
+
+                $dateCrea = new \DateTime('Now ', new \DateTimeZone('Europe/Paris'));
+                $fichier->setCreateFileAt($dateCrea);
+                $entityManager->persist($fichier);
+                $arme->setFichier($fichier);
+
+
+
+
+
             $entityManager->persist($arme);
             $entityManager->flush();
 
@@ -57,7 +86,7 @@ class ArmeController extends AbstractController
      */
     public function edit(Request $request, Arme $arme): Response
     {
-        $form = $this->createForm(Arme1Type::class, $arme);
+        $form = $this->createForm(ArmeType::class, $arme);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
--- src/Form/ArmeType.php
+++ src/Form/ArmeType.php
@@ -3,7 +3,15 @@
 namespace App\Form;
 
 use App\Entity\Arme;
+use App\Entity\Fichier;
+use App\Entity\Materiel;
+use App\Entity\Monnaie;
+use App\Entity\TypeArme;
+use App\Entity\TypeCategorie;
+use App\Entity\TypeDes;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,12 +26,32 @@ class ArmeType extends AbstractType
             ->add('beneficeMaluce')
             ->add('valeur')
             ->add('degat')
-            ->add('typeDes')
-            ->add('fichier')
-            ->add('monnaie')
-            ->add('typeArme')
-            ->add('typeCategorie')
-            ->add('materiel')
+            ->add('typeDes', EntityType::class,
+                array('class'=>TypeDes::class,
+                    'label'=>'Dés',
+                    'choice_label'=>'des')
+            )
+            ->add('imageAvInsertion', FileType::class            )
+            ->add('monnaie',EntityType::class
+            ,array('class'=>Monnaie::class,
+                    'label'=>'Monnaie',
+                    'choice_label'=>'nomMonnaie'))
+
+            ->add('typeArme', EntityType::class,
+                array('class'=>TypeArme::class,
+                    'label'=> "Type d'arme ",
+                    'choice_label'=>"nomTypeArme")
+            )
+            ->add('typeCategorie', EntityType::class,
+                array('class'=>TypeCategorie::class,
+                    'label'=>'Categorie',
+                    'choice_label'=>'categorie')
+            )
+            ->add('materiel', EntityType::class,
+                array('class'=>Materiel::class,
+                    'label'=>"Materiel de l'objet",
+                    'choice_label'=>'typeMateriel')
+            )
         ;
     }
 
