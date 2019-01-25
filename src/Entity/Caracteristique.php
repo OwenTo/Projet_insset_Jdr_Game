e385e808579215cb83b6c89b05777c3d74dc8941
Dylan Baldini <Dylan-baldini@outlook.fr>
2019-01-25 16:01:40 +0100

    ajout de la classe valeur carract + relation avec classe Caracteristique

--- src/Entity/Caracteristique.php
+++ src/Entity/Caracteristique.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\DiscriminatorColumn;
 use Doctrine\ORM\Mapping\DiscriminatorMap;
@@ -28,6 +30,16 @@ class Caracteristique
      */
     private $nomCaracteristique;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\ValeurCaract", mappedBy="caracteristique")
+     */
+    private $valeurCaracts;
+
+    public function __construct()
+    {
+        $this->valeurCaracts = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -41,6 +53,37 @@ class Caracteristique
     public function setNomCaracteristique(string $nomCaracteristique): self
     {
         $this->nomCaracteristique = $nomCaracteristique;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|ValeurCaract[]
+     */
+    public function getValeurCaracts(): Collection
+    {
+        return $this->valeurCaracts;
+    }
+
+    public function addValeurCaract(ValeurCaract $valeurCaract): self
+    {
+        if (!$this->valeurCaracts->contains($valeurCaract)) {
+            $this->valeurCaracts[] = $valeurCaract;
+            $valeurCaract->setCaracteristique($this);
+        }
+
+        return $this;
+    }
+
+    public function removeValeurCaract(ValeurCaract $valeurCaract): self
+    {
+        if ($this->valeurCaracts->contains($valeurCaract)) {
+            $this->valeurCaracts->removeElement($valeurCaract);
+            // set the owning side to null (unless already changed)
+            if ($valeurCaract->getCaracteristique() === $this) {
+                $valeurCaract->setCaracteristique(null);
+            }
+        }
 
         return $this;
     }
