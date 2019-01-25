dd016a3c63077df85e620afc81338eae7e52262f
Dylan Baldini <Dylan-baldini@outlook.fr>
2019-01-25 15:46:59 +0100

    ajout de la classe classePersonnage

---
+++ src/Entity/ClassePersonnage.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ClassePersonnageRepository")
+ */
+class ClassePersonnage
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nomClasse;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomClasse(): ?string
+    {
+        return $this->nomClasse;
+    }
+
+    public function setNomClasse(string $nomClasse): self
+    {
+        $this->nomClasse = $nomClasse;
+
+        return $this;
+    }
+}
