a98cd903181ea61cbee5d3257df3ed43d93bf666
Dylan Baldini <Dylan-baldini@outlook.fr>
2019-01-30 15:05:47 +0100

    ajout du coontroller et des pages  Races (personnage)

---
+++ src/Form/RaceType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Race;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RaceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nomRace')
+            ->add('descriptionRace')
+            ->add('typeRace')
+            ->add('capaciteRacials')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Race::class,
+        ]);
+    }
+}
