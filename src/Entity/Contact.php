b951ee1c8bd1d10f3ce9cefef38240de30a81280
Dylan Baldini <Dylan-baldini@outlook.fr>
2019-02-27 15:20:02 +0100

    creation de la classe Contact

---
+++ src/Entity/Contact.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nameContact;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Email()
+     */
+    private $emailContact;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min=10)
+
+     */
+    private $message;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNameContact(): ?string
+    {
+        return $this->nameContact;
+    }
+
+    public function setNameContact(string $nameContact): self
+    {
+        $this->nameContact = $nameContact;
+
+        return $this;
+    }
+
+    public function getEmailContact(): ?string
+    {
+        return $this->emailContact;
+    }
+
+    public function setEmailContact(string $emailContact): self
+    {
+        $this->emailContact = $emailContact;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
